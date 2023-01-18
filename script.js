7b2440f7e5bb2b64965b4a53c319c7e161db2190
Kalp Patel <kalp_patel@rocketmail.com>
2023-01-18 12:06:16 +0530

    vs-code-clone 90% completed

---
+++ script.js
@@ -0,0 +1,52 @@
+// let num = [1, 2, 3, 4, 5, "ABC", 8, 9, 10, "ABC"];
+
+// console.log(num.indexOf("ABC"));
+
+// let num = [1, 2, 3, 4, 5, "ABC", 8, 9, 10, "ABC"];
+
+// console.log(Array.isArray());
+
+// let arr1 = [1, 2, 3, 4, 5];
+// console.log(arr1.join(5));
+
+// console.log(arr1.pop());
+// console.log(arr1);
+
+// console.log(arr1);
+
+// console.log(arr1.reverse(14)); //exprement failed
+// console.log(arr1.shift());
+
+// let names = ["kalp", "patel"];
+// let upperLetter = [];
+
+// for (let upperLetters of fruit) {
+//   upperLetter.push();
+// }
+
+// function sum(val1, val2) {
+//   let result = val1 + val2;
+//   return result;
+// }
+
+// let add = sum(10, 25);
+// console.log(add);
+
+// let addition = sum(10, 65);
+// console.log(addition);
+
+let userName = {
+  firstName: "Kalp",
+  lastName: "patel",
+  role: "admin",
+  loginCount: 41,
+};
+
+const users = ["Kalp", "Jay", "Meet", "Tom", "Ted"];
+console.log(Array.isArray(users));
+
+let array4 = [1, 2, 3, 45, 6, 69, 75, 102];
+
+array4.fill("Anurag", 2, 4);
+
+console.log(array4);
